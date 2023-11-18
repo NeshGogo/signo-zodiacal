cb7c6712e76c0fe8293ecf4b53a23d820a45de71
NeshGogo <rafaelaguero10@hotmail.com>
2023-11-18 11:20:24 -0400

    migrate to dotnet 6

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,8 @@ namespace SignoZodiacal
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,12 +53,13 @@ namespace SignoZodiacal
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
-            app.UseMvc(routes =>
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=ZodiacalSigno}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
